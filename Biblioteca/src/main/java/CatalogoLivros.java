e0c29f9774816754ab54e3af50284d9718ad80d0
Isaque Melo <isaquegbmelo2@gmail.com>
2019-03-21 09:22:14 -0300

    biblioteca

---
+++ Biblioteca/src/main/java/CatalogoLivros.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class CatalogoLivros {
+    private List<Livro> livros = new ArrayList<Livro>();
+    private int quantidade_livros;
+
+    public void adicionarLivro(Livro livro){
+        this.livros.add(livro);
+    }
+
+    public void removerLivro(long id){
+        for (int i = 0; i < this.livros.size(); i++) {
+            Livro livro = this.livros.get(i);
+            if (livro.getId() == id){
+                livros.remove(livro);
+                break;
+            }
+        }
+    }
+
+    public Livro getLivro(long id){
+        for (int i = 0; i < this.livros.size(); i++) {
+            Livro livro = this.livros.get(i);
+            if (livro.getId() == id){
+                return livro;
+            }
+        }
+
+        throw new NullPointerException("Nao existe livro com esse id!");
+
+
+    }
+
+
+    public ArrayList<Livro> getLivros(){
+        return (ArrayList<Livro>) this.livros;
+    }
+
+    public void imprimeLivros(){
+        for (Livro livro : this.livros) {
+            System.out.printf("Nome: %s, ID: %d, Autor: %s\n\n", livro.getNome(), livro.getId(), livro.getAutor());
+        }
+    }
+
+}
